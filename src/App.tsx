a7296fc9fc73fb73dd12b250c31b2aafc2cd1c96
Gift Nnko <nnkogift@hotmail.com>
2022-04-02 19:43:51 +0300

    :sparkles: feat(Firebase): Firebase setup - Initial firebase setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {MantineProvider} from "@mantine/core";
 import MainRouter from "./modules/router";
-import { initializeApp } from "firebase/app";
+import {initializeApp} from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBe4pvR4iCVyLQNzTEqaLKoHSBegzHT64M",
-    authDomain: "uiss-demo.firebaseapp.com",
-    projectId: "uiss-demo",
-    storageBucket: "uiss-demo.appspot.com",
-    messagingSenderId: "62093310499",
-    appId: "1:62093310499:web:0ef2cb9dee5cff76fbcf96"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
